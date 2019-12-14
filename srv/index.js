28f66abec27c34eb82d5ef95d4b664864d4eab5f
Vinicius Martins <visimonemartins@hotmail.com>
2019-12-14 14:40:36 -0300

    Set user routes to srv

--- srv/index.js
+++ srv/index.js
@@ -8,6 +8,13 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const { userRouter } = require('../routes')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/user', userRouter)
+
 app.listen(3000, () => {
   console.log('Server running on 3000')
 })
